eaf8c6cc1f58c11171f74a43a742f2d0a704176c
Travis Weerts <travisaweerts@gmail.com>
2026-01-23 07:24:46 +0800

    demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,31 @@ export default function Home() {
           Enter a phone number to trigger an outbound call.
         </p>
       </div>
-      <CallAgent />
+      <div className="w-full max-w-2xl grid grid-cols-2 gap-4">
+        <CallAgent />
+        <div className="bg-gray-200 rounded-3xl p-4 text-sm">
+          <h2 className="text-2xl font-bold mb-4 text-center text-black">
+            Identify Info
+          </h2>
+          <p className="text-black">
+            <strong>Name:</strong> Homer Simpson
+          </p>
+          <p className="text-black">
+            <strong>Date of Birth:</strong> 12 May 1956
+          </p>
+          <p className="text-black">
+            <strong>Address:</strong> 742 Evergreen Terrace, Springfield, IL
+            62704
+          </p>
+
+          <p className="text-black">
+            <strong>Debt:</strong> $250 with Koala Bank, due on 1 August 2024
+          </p>
+          <p className="text-black">
+            <strong>Minimum payment plan amount:</strong> $25
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
